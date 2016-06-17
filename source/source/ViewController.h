34bd4a3e380e49b698e021dade7488589588b9c2
GitHubW <1915320086@qq.com>
2016-06-17 17:36:53 +0800

    提交任务

---
+++ source/source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  source
+//
+//  Created by wb on 16/6/17.
+//  Copyright © 2016年 北京尚学堂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ source/source/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  source
+//
+//  Created by wb on 16/6/17.
+//  Copyright © 2016年 北京尚学堂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
